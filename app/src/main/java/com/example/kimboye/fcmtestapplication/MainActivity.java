11d45cbd1aed596db8d9b497dea49b53cfe4d134
boye <boyepp@gmail.com>
2018-01-09 16:59:51 +0900

    Init

---
+++ app/src/main/java/com/example/kimboye/fcmtestapplication/MainActivity.java
@@ -0,0 +1,137 @@
+package com.example.kimboye.fcmtestapplication;
+
+import android.app.DownloadManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    TextView text;
+    EditText editText;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final RequestQueue queue = Volley.newRequestQueue(this);  // this = context
+
+        text = findViewById(R.id.text);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                final String url = "http://172.30.1.60:3000/send/boye/" + editText.getText().toString();
+
+                // prepare the Request
+                JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, (String) null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                // display response
+                                Log.d("Response", response.toString());
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.d("Error.Response", error.toString());
+                            }
+                        }
+                );
+
+                // add it to the RequestQueue
+                queue.add(getRequest);
+
+
+                //POST로 보내기
+//                final String url = "http://172.30.1.2:3000/send/kim/hihihi";
+//                StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+//                        new Response.Listener<String>()
+//                        {
+//                            @Override
+//                            public void onResponse(String response) {
+//                                // response
+//                                Log.d("Response", response);
+//                            }
+//                        },
+//                        new Response.ErrorListener()
+//                        {
+//                            @Override
+//                            public void onErrorResponse(VolleyError error) {
+//                                // error
+//                                Log.d("Error.Response", error.toString());
+//                            }
+//                        }
+//                ) {
+//                    @Override
+//                    protected Map<String, String> getParams()
+//                    {
+//                        Map<String, String>  params = new HashMap<String, String>();
+//                        params.put("name", "boye");
+//                        params.put("body", editText.getText().toString());
+//
+//                        return params;
+//                    }
+//                };
+//                queue.add(postRequest);
+
+            }
+        });
+
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+
+        // 알림센터에서 푸시누르면 Intent에 data담겨있음. 이거 실행됨
+        // data Intent에 담겨있음
+        if (getIntent().getExtras() != null) {
+
+            String message = getIntent().getExtras().getString("msg");
+            text.append(message);
+
+        }
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/kimboye/fcmtestapplication/MyFirebaseMessagingService.java
@@ -0,0 +1,58 @@
+package com.example.kimboye.fcmtestapplication;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by kimboye on 2018. 1. 7..
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = "MyFirebaseMsgService";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        String message = "";
+
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        message += remoteMessage.getFrom() + " : ";
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+            message += remoteMessage.getData().get("msg") + "\n";
+
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("msg", message);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+
+        startActivity(intent);
+
+
+    }
+    // [END receive_message]
+
+
+}
